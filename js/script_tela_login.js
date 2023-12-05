dba8a6418d02a441fcb8c3c5723dd14c7ad0c89d
Gutasss <gutmota23@gmail.com>
2023-12-05 12:02:50 -0300

    alteracao do botao

--- js/script_tela_login.js
+++ js/script_tela_login.js
@@ -1,20 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
+    usuarioInput = document.getElementById('usuario');
+    senhaInput = document.getElementById('senha');
+    botaoPronto = document.getElementById('botaoPronto');
+    loading = document.querySelector('.loading');
 
-    function before() {
-        loading = document.querySelector('.loading')
-        loading.classList.remove('hidden')
+    function atualizarEstadoDoBotao() {
+        if (usuarioInput.value && senhaInput.value) {
+            botaoPronto.style.opacity = '1';
+            botaoPronto.style.pointerEvents = 'auto';
+        } else {
+            botaoPronto.style.opacity = '0.5';
+            botaoPronto.style.pointerEvents = 'none';
+        }
     }
 
-    pronto = document.querySelector('.pronto')
+    usuarioInput.addEventListener('input', atualizarEstadoDoBotao);
+    senhaInput.addEventListener('input', atualizarEstadoDoBotao);
 
-    pronto.addEventListener('click', function(event) {
-        event.preventDefault();
-        
-        before()
+    function before() {
+        loading.classList.remove('hidden');
+    }
 
-        setTimeout(function() {
-            window.location.href = "tela_principal.html";
-        }, 1500);
-    })
+    botaoPronto.addEventListener('click', function(event) {
+        if (botaoPronto.style.opacity === '0.5') {
+            event.preventDefault();
+        } else {
+            before();
+            setTimeout(function() {
+                window.location.href = "tela_principal.html";
+            }, 1500);
+        }
+    });
 
-})
+    botaoPronto.style.opacity = '0.5';
+    botaoPronto.style.pointerEvents = 'none';
+
+    atualizarEstadoDoBotao();
+});
